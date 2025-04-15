73ab269ebe3e16207936b27a4e68981721efce74
Grupo Q <ignacioquercetti@gmail.com>
2025-04-15 13:17:13 -0300

    Agregado de limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,6 +1,3 @@
-import re  
-
-def is_palindrome(Text):
-   
-    cleaned_text = re.sub(r'[^a-zA-Z0-9]', '', Text).lower()
-    return cleaned_text == cleaned_text[::-1]
+def is_palindrome(text):
+    cleaned_text = ''.join(char.lower() for char in text if char.isalnum())
+    pass
